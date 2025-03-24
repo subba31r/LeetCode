f5da2a74a8d5ef2ce18e63021d7052425197fe9a
subba31r <60545532+subba31r@users.noreply.github.com>
2025-03-23 22:19:50 -0400

    Time: 159 ms (72.04%) | Memory: 52.9 MB (55.42%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.py
+++ 3430-count-days-without-meetings/count-days-without-meetings.py
@@ -1,19 +1,29 @@
 class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        # meetings.sort()
+        # # print(meetings)
+        # temp = [meetings[0]]
+        # for st, en in meetings:
+        #     pre = temp[-1][1]
+        #     if st<=pre:
+        #         temp[-1][1] = max(pre,en)
+        #     else:
+        #         temp.append([st,en])
+        # # print(temp)
+        # pre = temp[0][1]
+        # count = temp[0][0] - 1
+        # for i in range(1,len(temp)):
+        #     count = count + (temp[i][0]-pre-1)
+        #     pre = temp[i][1]
+        # count = count + (days-pre)
+        # return count
+
         meetings.sort()
-        # print(meetings)
-        temp = [meetings[0]]
+        count = 0
+        pre = 0
         for st, en in meetings:
-            pre = temp[-1][1]
-            if st<=pre:
-                temp[-1][1] = max(pre,en)
-            else:
-                temp.append([st,en])
-        # print(temp)
-        pre = temp[0][1]
-        count = temp[0][0] - 1
-        for i in range(1,len(temp)):
-            count = count + (temp[i][0]-pre-1)
-            pre = temp[i][1]
-        count = count + (days-pre)
+            if st > pre+1:
+                count += st-pre-1
+            pre = max(pre,en)
+        count += days-pre
         return count
