2bffa22a107a6657f9f8abf533fd65cff05a6a8a
subba31r <60545532+subba31r@users.noreply.github.com>
2025-03-28 00:05:38 -0400

    Time: 8 ms (52.54%) | Memory: 18.4 MB (31.32%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.py
+++ 437-path-sum-iii/path-sum-iii.py
@@ -6,28 +6,46 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        self.res = [0]
+        #brute force - dfs on all nodes and dfs on every node path
+        # self.res = [0]
 
-        def path(curSum,root,li):
-            if root is None:
-                return
+        # def path(curSum,root,li):
+        #     if root is None:
+        #         return
 
-            curSum = curSum + root.val
-            li.append(root.val)
-            if curSum == targetSum:  
-                self.res[0] += 1
+        #     curSum = curSum + root.val
+        #     li.append(root.val)
+        #     if curSum == targetSum:  
+        #         self.res[0] += 1
 
-            path(curSum,root.left,li)
-            path(curSum,root.right,li)
-            li.pop(-1)    
+        #     path(curSum,root.left,li)
+        #     path(curSum,root.right,li)
+        #     li.pop(-1)    
         
-        def dfs(root):
-            if not root:
-                return
-            path(0,root,[])
-            dfs(root.left)
-            dfs(root.right)
+        # def dfs(root):
+        #     if not root:
+        #         return
+        #     path(0,root,[])
+        #     dfs(root.left)
+        #     dfs(root.right)
             
-        # path(0,root,[])
-        dfs(root)
-        return self.res[0]
+        # # path(0,root,[])
+        # dfs(root)
+        # return self.res[0]
+        #
+
+        #optimized is to keep track of prefix sum
+        prefixSum = {0:1}
+        def dfs(root,cur):
+            if root is None:
+                return 0
+            cur = cur+root.val
+            total = prefixSum.get(cur-targetSum,0) #count no.of paths ending at the current node
+            prefixSum[cur] = prefixSum.get(cur,0)+1
+            total += dfs(root.left,cur)
+            total += dfs(root.right,cur)
+
+            prefixSum[cur] -= 1 #backtrack to remove the current cumulative sum from hashmap
+            return total
+        return dfs(root,0)
+
