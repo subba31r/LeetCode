2d6c338d520cc25024748dd2a23b77c041b0b01a
subba31r <60545532+subba31r@users.noreply.github.com>
2025-04-24 15:27:48 -0400

    Time: 14 ms (46.41%) | Memory: 18.1 MB (33.33%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,35 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
-        #If both strings are not of same length
-        # if len(s) != len(t):
-        #     return False
-        # dic = {}
-        # for i in range(0,len(s)):
-        #     if s[i] in dic:
-        #         dic[s[i]] += 1
-        #     else:
-        #         dic[s[i]] = 1
-            
-        #     if t[i] in dic:
-        #         dic[t[i]] -= 1
-        #     else:
-        #         dic[t[i]] = -1
-        
-        # for k in dic:
-        #     if dic[k] != 0:
-        #         return False
-        
-        # return True
-
         if len(s) != len(t):
             return False
+        hm = {}
+        for c in s:
+            if c not in hm:
+                hm[c] = 0
+            hm[c] += 1
         
-        sSet = set(s)
-        for i in sSet:
-            if s.count(i) != t.count(i):
-                return False
-        return True
-
+        for c in t:
+            if c not in hm:
+                hm[c] = 1
+            else:
+                hm[c] -= 1
+        
+        
+        return min(hm.values()) == max(hm.values()) == 0
 
         
