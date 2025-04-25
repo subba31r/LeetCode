b21769db4111e7777a77fbc109bffccfc739447d
subba31r <60545532+subba31r@users.noreply.github.com>
2025-04-24 21:10:25 -0400

    Time: 15 ms (10.07%) | Memory: 24.1 MB (19.84%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,12 +1,31 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # hm = {}
+        # for num in nums:
+        #     if num not in hm:
+        #         hm[num] = 0
+        #     hm[num] += 1
+        # sortedHm = sorted(hm.items(),key=lambda x:x[1],reverse=True)
+        # res = []
+        # for i in range(k):
+        #     res.append(sortedHm[i][0])
+        # return res
         hm = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
         for num in nums:
             if num not in hm:
                 hm[num] = 0
             hm[num] += 1
-        sortedHm = sorted(hm.items(),key=lambda x:x[1],reverse=True)
+
+        for num, cnt in hm.items():
+            freq[cnt].append(num)
+
+
         res = []
-        for i in range(k):
-            res.append(sortedHm[i][0])
-        return res
+        for i in range(len(freq)-1,0,-1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+        
