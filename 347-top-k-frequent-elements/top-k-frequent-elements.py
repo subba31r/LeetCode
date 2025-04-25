66d34284c9e20a7db1c4ab86e34fe3bef9a6dd8b
subba31r <60545532+subba31r@users.noreply.github.com>
2025-04-24 21:05:07 -0400

    Time: 6 ms (55.80%) | Memory: 21.2 MB (62.00%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,20 +1,12 @@
-from collections import Counter
-from heapq import nlargest
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # dic = {}
-        # for n in nums:
-        #     if n not in dic:
-        #         dic[n] = 1
-        #     else:
-        #         dic[n] += 1
-        # sorted_dic = {
-        #     k:v for k,v in sorted(dic.items(),
-        #     key=lambda item:item[1],reverse=True)
-        # }
-
-        # key = list(sorted_dic.keys())
-        # return key[:k]
-        count = Counter(nums) #we can use this for counting occurences
-        return nlargest(k, count.keys(), key=count.get)  
-        #count.get ensures that sorting is done based on frequency, not the number itself
+        hm = {}
+        for num in nums:
+            if num not in hm:
+                hm[num] = 0
+            hm[num] += 1
+        sortedHm = sorted(hm.items(),key=lambda x:x[1],reverse=True)
+        res = []
+        for i in range(k):
+            res.append(sortedHm[i][0])
+        return res
