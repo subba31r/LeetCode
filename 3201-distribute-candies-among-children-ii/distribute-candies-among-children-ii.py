9c61552231f1c506a7ec1522b25a278a67b9143f
subba31r <60545532+subba31r@users.noreply.github.com>
2025-06-01 19:30:49 -0400

    Time: 4719 ms (8.96%) | Memory: 17.4 MB (98.62%) - LeetSync

---
+++ 3201-distribute-candies-among-children-ii/distribute-candies-among-children-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def distributeCandies(self, n: int, limit: int) -> int:
+        res = 0
+        for i in range(0,min(limit,n)+1):
+            if n-i > 2*limit:
+                continue
+            res += min(n-i,limit) - max(0,n-i-limit)+1
+        return res
